bb65b3959af9b8d9d73f935d8f828dccc15caf58
KiwiKey <kiwikey.vn@gmail.com>
2022-10-20 22:19:24 +0700

    Update Kiwi111S
    Add OLED 128*64 animation: BongoCat (for NguyenDuyThanh's custom keyboard)
    Add OLED's mode configuration in EEPROM. OLED screen will run last mode before turning off.

--- keyboards/kiwikey/kiwi111s/keymaps/via-oled/config.h
+++ keyboards/kiwikey/kiwi111s/keymaps/via-oled/config.h
@@ -25,3 +25,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define OLED_DISPLAY_128X64
 #define OLED_IC OLED_IC_SH1106
 #define OLED_COLUMN_OFFSET 2
+
+#define VIA_EEPROM_CUSTOM_CONFIG_SIZE 1
