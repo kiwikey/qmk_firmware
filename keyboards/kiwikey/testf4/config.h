f8e8e4226ab77e87f6ad8da4875592205557179b
KiwiKey <kiwikey.vn@gmail.com>
2023-03-18 19:53:48 +0700

    Update config.h

--- keyboards/kiwikey/testf4/config.h
+++ keyboards/kiwikey/testf4/config.h
@@ -11,11 +11,11 @@
 #define SPI_MOSI_PAL_MODE 5
 #define SPI_MISO_PAL_MODE 5
 
-#define DISPLAY_CS_PIN      B9
-#define DISPLAY_DC_PIN      B8
-#define DISPLAY_RST_PIN     B7
-#define DISPLAY_SPI_DIVISOR 8
-#define DISPLAY_SPI_MODE    2 // ST7789 240*240 works with mode 2 or 3 only, not sure why
+#define DISPLAY_CS_PIN      B9 // cannot be changed
+#define DISPLAY_DC_PIN      B12
+#define DISPLAY_RST_PIN     B14
+#define DISPLAY_SPI_DIVISOR 1 // 1, 2, 4, 8 all works
+#define DISPLAY_SPI_MODE    3 // ST7789 240*240 works with mode 2 or 3 only, not sure why
 
 #define QUANTUM_PAINTER_NUM_IMAGES	8	//The maximum number of images/animations that can be loaded at any one time.
 #define QUANTUM_PAINTER_NUM_FONTS	4	//The maximum number of fonts that can be loaded at any one time.
@@ -24,7 +24,6 @@
 #define QUANTUM_PAINTER_PIXDATA_BUFFER_SIZE	32	//The limit of the amount of pixel data that can be transmitted in one transaction to the display. Higher values require more RAM on the MCU.
 #define QUANTUM_PAINTER_SUPPORTS_256_PALETTE	FALSE	//If 256-color palettes are supported. Requires significantly more RAM on the MCU.
 //#define QUANTUM_PAINTER_DEBUG	unset	//Prints out significant amounts of debugging information to CONSOLE output. Significant performance degradation, use only for debugging.
-
 
 #define TAP_CODE_DELAY 10
 /* Mechanical locking support. Use KC_LCAP", "KC_LNUM or KC_LSCR instead in keymap */
