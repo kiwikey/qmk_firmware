7956d7b23cf43129ae2999ac497880f39045686c
KiwiKey <kiwikey.vn@gmail.com>
2023-04-17 17:53:13 +0700

    Add Sport48
    48-key Split Ortholinear keyboard:
    + 2 TFT screens
    + 2 rotary encoders
    Using BlackPill F401 as main controller.
    2023.04.17: almost all functions work fine.
    TODO:
    + Add UI
    + Add external EEPROM to see what can I do with it

---
+++ keyboards/kiwikey/sport48/keymaps/via/rules.mk
@@ -0,0 +1,11 @@
+ENCODER_MAP_ENABLE = yes
+VIA_ENABLE = yes
+
+VPATH += keyboards/kiwikey/sport48/graphics
+SRC += \
+    sport48_graphics.c \
+	thintel15.qff.c \
+	arial30.qff.c \
+	pikachu.qgf.c \
+	anya01.qgf.c \
+	pusheen_240px.qgf.c
---
+++ keyboards/kiwikey/sport48/mcuconf.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef STM32_SPI_USE_SPI2
+#define STM32_SPI_USE_SPI2 TRUE
+
+#undef STM32_SERIAL_USE_USART1
+#define STM32_SERIAL_USE_USART1 TRUE
+
+//#undef STM32_I2C_USE_I2C1
+//#define STM32_I2C_USE_I2C1 TRUE
+
+/*
+#undef STM32_PWM_USE_TIM1
+#define STM32_PWM_USE_TIM1 TRUE
+#undef STM32_GPT_USE_TIM4
+#define STM32_GPT_USE_TIM4 TRUE
+*/
