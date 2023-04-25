80e9148c69bfafd2f58cbcbf301d2e0162d47e37
KiwiKey <kiwikey.vn@gmail.com>
2023-04-25 22:34:32 +0700

    Update Kiwi65S
    Just cleaning codes

--- keyboards/kiwikey/kiwi65s/config.h
+++ keyboards/kiwikey/kiwi65s/config.h
@@ -1,9 +1,4 @@
 #pragma once
 
-/*** SPLIT DEFINITIONS ***/
 #define EE_HANDS // Handedness by EEPROM
-
-#define USE_SERIAL
-//#define SOFT_SERIAL_PIN D2
 #define SPLIT_TRANSPORT_MIRROR
-//#define SPLIT_USB_DETECT
