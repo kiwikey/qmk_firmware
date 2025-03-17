6fce9cdc4ebd7dc9f8c4a7e7b7651d1a14045d24
KiwiKey <kiwikey.vn@gmail.com>
2025-03-17 14:10:13 +0700

    Temp3

---
+++ keyboards/kiwikey/kiwi5x5/keymaps/via1/config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define TAPPING_TERM 300
+// #define TAPPING_TERM_PER_KEY // to customize TAPPING_TERM for Tap-dance, not affected other functions
---
+++ keyboards/kiwikey/kiwi5x5/rules.mk
@@ -0,0 +1,49 @@
+QUANTUM_PAINTER_DRIVERS += st7789_spi
+SEND_STRING_ENABLE = yes
+
+VPATH += keyboards/kiwikey/0_libraries/custom_display
+
+SRC += \
+	encoder.c         \
+	rgb_matrix_name.c \
+    qp_graphics.c     \
+	qp/qp_custom_api.c     \
+	qp/qp_includes.c       \
+	qp/qp_menu.c           \
+	qp/qp_widget_layer.c   \
+	qp/qp_widget_matrix.c  \
+	qp/qp_widget_rgbstat.c \
+	qp/qp_widget_encoder.c
+		# via_custom.c      \
+
+# FONTS
+SRC += \
+	qp/resources/thintel15.qff.c    \
+	qp/resources/roboto20.qff.c     \
+	qp/resources/roboto25.qff.c     \
+	qp/resources/robotobold25.qff.c
+
+# IMAGES & GIFS
+# SRC += \
+	# qp/resources/pikachu.qgf.c       \
+	# qp/resources/anya01.qgf.c        \
+	# qp/resources/pusheen_240px.qgf.c \
+	# qp/resources/gif_nyan120px.qgf.c \
+	# qp/resources/gif_cat01.qgf.c     \
+	# qp/resources/gif_cat02.qgf.c     \
+	# qp/resources/gif_dog01.qgf.c
+
+# ICONS
+SRC += \
+	qp/resources/icons/ico32_brightness.qgf.c  \
+	qp/resources/icons/ico16_arrow_up.qgf.c    \
+	qp/resources/icons/ico16_arrow_down.qgf.c  \
+	qp/resources/icons/ico16_arrow_left.qgf.c  \
+	qp/resources/icons/ico16_arrow_right.qgf.c
+	
+	# qp/resources/icons/lock-caps-ON.qgf.c \
+	# qp/resources/icons/lock-scrl-ON.qgf.c \
+	# qp/resources/icons/lock-num-ON.qgf.c \
+	# qp/resources/icons/lock-caps-OFF.qgf.c \
+	# qp/resources/icons/lock-scrl-OFF.qgf.c \
+	# qp/resources/icons/lock-num-OFF.qgf.c \
