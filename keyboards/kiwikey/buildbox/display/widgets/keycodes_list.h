13eb4ab3e7fef2d4e3f480f0ffd2c1636c12e55e
KiwiKey <75843996+kiwikey@users.noreply.github.com>
2026-07-01 18:46:03 +0700

    Stable02

---
+++ keyboards/kiwikey/buildbox/display/widgets/keycodes_list.h
@@ -0,0 +1,97 @@
+/*
+	This file is generated from converted_data.xlsx
+	Raw data from qmk_firmware/quantum/keycodes.h
+*/
+
+//  KEY (  Keycode        , Hex   ,    Text String   )
+
+#define KEYCODE_LIST \
+    X (  KC_NO               , 0x0000,    "NO"     ) \
+    X (  KC_TRANSPARENT      , 0x0001,    "TRANS"     ) \
+    X (  KC_A                , 0x0004,    "A"     ) \
+    X (  KC_B                , 0x0005,    "B"     ) \
+    X (  KC_C                , 0x0006,    "C"     ) \
+    X (  KC_D                , 0x0007,    "D"     ) \
+    X (  KC_E                , 0x0008,    "E"     ) \
+    X (  KC_F                , 0x0009,    "F"     ) \
+    X (  KC_G                , 0x000A,    "G"     ) \
+    X (  KC_H                , 0x000B,    "H"     ) \
+    X (  KC_I                , 0x000C,    "I"     ) \
+    X (  KC_J                , 0x000D,    "J"     ) \
+    X (  KC_K                , 0x000E,    "K"     ) \
+    X (  KC_L                , 0x000F,    "L"     ) \
+    X (  KC_M                , 0x0010,    "M"     ) \
+    X (  KC_N                , 0x0011,    "N"     ) \
+    X (  KC_O                , 0x0012,    "O"     ) \
+    X (  KC_P                , 0x0013,    "P"     ) \
+    X (  KC_Q                , 0x0014,    "Q"     ) \
+    X (  KC_R                , 0x0015,    "R"     ) \
+    X (  KC_S                , 0x0016,    "S"     ) \
+    X (  KC_T                , 0x0017,    "T"     ) \
+    X (  KC_U                , 0x0018,    "U"     ) \
+    X (  KC_V                , 0x0019,    "V"     ) \
+    X (  KC_W                , 0x001A,    "W"     ) \
+    X (  KC_X                , 0x001B,    "X"     ) \
+    X (  KC_Y                , 0x001C,    "Y"     ) \
+    X (  KC_Z                , 0x001D,    "Z"     ) \
+    X (  KC_1                , 0x001E,    "1"     ) \
+    X (  KC_2                , 0x001F,    "2"     ) \
+    X (  KC_3                , 0x0020,    "3"     ) \
+    X (  KC_4                , 0x0021,    "4"     ) \
+    X (  KC_5                , 0x0022,    "5"     ) \
+    X (  KC_6                , 0x0023,    "6"     ) \
+    X (  KC_7                , 0x0024,    "7"     ) \
+    X (  KC_8                , 0x0025,    "8"     ) \
+    X (  KC_9                , 0x0026,    "9"     ) \
+    X (  KC_0                , 0x0027,    "0"     ) \
+    X (  KC_ENTER            , 0x0028,    "Ent"     ) \
+    X (  KC_ESCAPE           , 0x0029,    "Esc"     ) \
+    X (  KC_BACKSPACE        , 0x002A,    "BKSP"     ) \
+    X (  KC_TAB              , 0x002B,    "Tab"     ) \
+    X (  KC_SPACE            , 0x002C,    "_"     ) \
+    X (  KC_MINUS            , 0x002D,    "-"     ) \
+    X (  KC_EQUAL            , 0x002E,    "="     ) \
+    X (  KC_F1               , 0x003A,    "F1"     ) \
+    X (  KC_F2               , 0x003B,    "F2"     ) \
+    X (  KC_F3               , 0x003C,    "F3"     ) \
+    X (  KC_F4               , 0x003D,    "F4"     ) \
+    X (  KC_F5               , 0x003E,    "F5"     ) \
+    X (  KC_F6               , 0x003F,    "F6"     ) \
+    X (  KC_F7               , 0x0040,    "F7"     ) \
+    X (  KC_F8               , 0x0041,    "F8"     ) \
+    X (  KC_F9               , 0x0042,    "F9"     ) \
+    X (  KC_F10              , 0x0043,    "F10"     ) \
+    X (  KC_F11              , 0x0044,    "F11"     ) \
+    X (  KC_F12              , 0x0045,    "F12"     ) \
+    X (  QK_MACRO_0          , 0x7700,    "M0"     ) \
+    X (  QK_MACRO_1          , 0x7701,    "M1"     ) \
+    X (  QK_MACRO_2          , 0x7702,    "M2"     ) \
+    X (  QK_MACRO_3          , 0x7703,    "M3"     ) \
+    X (  QK_MACRO_4          , 0x7704,    "M4"     ) \
+    X (  QK_MACRO_5          , 0x7705,    "M5"     ) \
+    X (  QK_MACRO_6          , 0x7706,    "M6"     ) \
+    X (  QK_MACRO_7          , 0x7707,    "M7"     ) \
+    X (  QK_MACRO_8          , 0x7708,    "M8"     ) \
+    X (  QK_MACRO_9          , 0x7709,    "M9"     ) \
+    X (  QK_MACRO_10         , 0x770A,    "M10"     ) \
+    X (  QK_MACRO_11         , 0x770B,    "M11"     ) \
+    X (  QK_MACRO_12         , 0x770C,    "M12"     ) \
+    X (  QK_MACRO_13         , 0x770D,    "M13"     ) \
+    X (  QK_MACRO_14         , 0x770E,    "M14"     ) \
+    X (  QK_MACRO_15         , 0x770F,    "M15"     ) \
+    X (  QK_MACRO_16         , 0x7710,    "M16"     ) \
+    X (  QK_MACRO_17         , 0x7711,    "M17"     ) \
+    X (  QK_MACRO_18         , 0x7712,    "M18"     ) \
+    X (  QK_MACRO_19         , 0x7713,    "M19"     ) \
+    X (  QK_MACRO_20         , 0x7714,    "M20"     ) \
+    X (  QK_MACRO_21         , 0x7715,    "M21"     ) \
+    X (  QK_MACRO_22         , 0x7716,    "M22"     ) \
+    X (  QK_MACRO_23         , 0x7717,    "M23"     ) \
+    X (  QK_MACRO_24         , 0x7718,    "M24"     ) \
+    X (  QK_MACRO_25         , 0x7719,    "M25"     ) \
+    X (  QK_MACRO_26         , 0x771A,    "M26"     ) \
+    X (  QK_MACRO_27         , 0x771B,    "M27"     ) \
+    X (  QK_MACRO_28         , 0x771C,    "M28"     ) \
+    X (  QK_MACRO_29         , 0x771D,    "M29"     ) \
+    X (  QK_MACRO_30         , 0x771E,    "M30"     ) \
+    X (  QK_MACRO_31         , 0x771F,    "M31"     )
