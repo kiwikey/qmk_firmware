46a9810598a68ce7abd7473a1a9311810a73c9a6
KiwiKey <75843996+kiwikey@users.noreply.github.com>
2026-07-13 00:12:27 +0700

    Update keymap.c

--- keyboards/kiwikey/buildbox/keymaps/default/keymap.c
+++ keyboards/kiwikey/buildbox/keymaps/default/keymap.c
@@ -1,13 +1,13 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
 
+#include "display/defines.h" // custom keycodes are defined here
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_numpad_4x4(
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_A,
-        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        KC_P0,   KC_C,    KC_PDOT, KC_D
+    [0] = LAYOUT_numpad_4x4 (
+        KC_P7,   KC_P8,   KC_P9,   KC_KP_PLUS,
+        KC_P4,   KC_P5,   KC_P6,   KC_KP_MINUS,
+        KC_P1,   KC_P2,   KC_P3,   KC_KP_ASTERISK,
+        KC_P0,   KC_PDOT, KC_PENT, KC_KP_SLASH,
+		KC_NEXT_LAYER, KC_MY_MENU
     )
 };
